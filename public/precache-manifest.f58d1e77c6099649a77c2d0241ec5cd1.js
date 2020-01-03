8cf473fa595500f33d9019ea89d9b4035fd08217
Giftson John Donald <giftson@socure.me>
2020-01-03 12:59:55 +0530

    changes

---
+++ public/precache-manifest.f58d1e77c6099649a77c2d0241ec5cd1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c63508f8c55711a9aeceec083a78aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd4d9484e48db823b90c",
+    "url": "/static/css/main.eb6f1f40.chunk.css"
+  },
+  {
+    "revision": "2dc05e8f050ee8c1b3a5",
+    "url": "/static/js/2.1f8789df.chunk.js"
+  },
+  {
+    "revision": "9d018a588c0ff665aaccbcad4ee6c16e",
+    "url": "/static/js/2.1f8789df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd4d9484e48db823b90c",
+    "url": "/static/js/main.2f718b82.chunk.js"
+  },
+  {
+    "revision": "1e89d761531d42589fd1",
+    "url": "/static/js/runtime-main.38080af7.js"
+  },
+  {
+    "revision": "a05855879df8aa9113426b659d1d7cd2",
+    "url": "/static/media/Shaastra.a0585587.png"
+  },
+  {
+    "revision": "16b2c5676aa6363d0390d172cfac0a4c",
+    "url": "/static/media/happy_smiley.16b2c567.png"
+  },
+  {
+    "revision": "d552c452feb59f70510d705b65d29800",
+    "url": "/static/media/socure.d552c452.png"
+  },
+  {
+    "revision": "76096452cd35edc354d61977784f708e",
+    "url": "/static/media/wrong.76096452.png"
+  }
+]);
